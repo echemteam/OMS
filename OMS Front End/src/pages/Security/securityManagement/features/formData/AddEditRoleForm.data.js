6164df23d6b42864749a676ba515d51fe72aa541
kirtan <kirtan.p@mol-tech.com>
2024-06-03 15:57:05 +0530

    remove copy action from the table

--- OMS Front End/src/pages/Security/securityManagement/features/formData/AddEditRoleForm.data.js
+++ OMS Front End/src/pages/Security/securityManagement/features/formData/AddEditRoleForm.data.js
@@ -41,10 +41,6 @@ export const SecurityRoleGridConfig = {
       },
       customAction: [
         {
-          name: "COPY",
-          iconName: AppIcons.copyIcon,
-        },
-        {
           name: "PERMISSION",
           iconName: AppIcons.permissionIcon,
         },
