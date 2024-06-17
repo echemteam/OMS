e89b0efdb351d8b59165818e492814a231f90a10
shrutenparmar <shruten.p@mol-tech.com>
2024-06-17 11:16:59 +0530

    changes.

--- OMS Front End/src/pages/customerDetail/ViewCustomer.js
+++ OMS Front End/src/pages/customerDetail/ViewCustomer.js
@@ -23,7 +23,7 @@ const ViewCustomer = () => {
   const [isModelOpen, setisModelOpen] = useState(false);
   const [customerData, setCustomerData] = useState(null)
 
-  const {  setCustomerId } = useContext(BasicDetailContext);
+  const { setCustomerId } = useContext(BasicDetailContext);
 
   const [getCustomersBasicInformationById,
     {
@@ -88,55 +88,55 @@ const ViewCustomer = () => {
         </div>
       ),
     },
-    {
-      sMenuItemCaption: "Notes",
-      component: (
-        <div className="mt-2">
-          <NotesDetail />
-        </div>
-      ),
-    },
-    {
-      sMenuItemCaption: "History",
-      component: (
-        <div className="">
-          <HistoryDetail />
-        </div>
-      ),
-    },
+    // {
+    //   sMenuItemCaption: "Notes",
+    //   component: (
+    //     <div className="mt-2">
+    //       <NotesDetail />
+    //     </div>
+    //   ),
+    // },
+    // {
+    //   sMenuItemCaption: "History",
+    //   component: (
+    //     <div className="">
+    //       <HistoryDetail />
+    //     </div>
+    //   ),
+    // },
   ];
 
   return (
     <>
-        <div className="card-bottom-m-0">
-          <div className="row">
-            <div className="col-xxl-4 col-xl-4 col-md-3 col-12 basic-left-part customer-desc-left-sec">
-              <CardSection>
-                <CustomerDetails editClick={handleToggleModal} customerData={customerData} />
-              </CardSection>
-            </div>
-            <div className="col-xxl-8 col-xl-8 col-md-9 col-12 other-info-tab">
-              <CardSection
-                cardTitle="Other Information"
-              // buttonClassName="theme-button"
-              // rightButton={true}
-              // buttonText="Save"
-              // titleButtonClick={onEditAction}
-              >
-                <RenderTabs tabs={tabs} />
-              </CardSection>
-            </div>
+      <div className="card-bottom-m-0">
+        <div className="row">
+          <div className="col-xxl-4 col-xl-4 col-md-3 col-12 basic-left-part customer-desc-left-sec">
+            <CardSection>
+              <CustomerDetails editClick={handleToggleModal} customerData={customerData} />
+            </CardSection>
+          </div>
+          <div className="col-xxl-8 col-xl-8 col-md-9 col-12 other-info-tab">
+            <CardSection
+              cardTitle="Other Information"
+            // buttonClassName="theme-button"
+            // rightButton={true}
+            // buttonText="Save"
+            // titleButtonClick={onEditAction}
+            >
+              <RenderTabs tabs={tabs} />
+            </CardSection>
           </div>
         </div>
-        <SidebarModel
-          modalTitle="Edit Basic Information"
-          contentClass="content-65 basic-info-model"
-          onClose={onSidebarClose}
-          modalTitleIcon={AppIcons.AddIcon}
-          isOpen={isModelOpen}
-        >
-          <BasicDetail onSidebarClose={onSidebarClose} isOpen={isModelOpen} customerData={customerData} pageId={pageId} onhandleRepeatCall={handleRepeatCall}/>
-        </SidebarModel>
+      </div>
+      <SidebarModel
+        modalTitle="Edit Basic Information"
+        contentClass="content-65 basic-info-model"
+        onClose={onSidebarClose}
+        modalTitleIcon={AppIcons.AddIcon}
+        isOpen={isModelOpen}
+      >
+        <BasicDetail onSidebarClose={onSidebarClose} isOpen={isModelOpen} customerData={customerData} pageId={pageId} onhandleRepeatCall={handleRepeatCall} />
+      </SidebarModel>
     </>
   );
 };
