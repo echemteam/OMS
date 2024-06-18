df1af513635ee11e2ef7ae2a578363f1781c6fcf
Amit Sonigara <amit.s@mol-tech.com>
2024-06-18 18:30:45 +0530

    Create an API to Un Freeze/Un block/ enable suplier.

---
+++ OMS.API/OMS.Domain.Entities/API/Request/Supplier/UpdateSupplierStatusRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace OMS.Domain.Entities.API.Request.Supplier
+{
+    public class UpdateSupplierStatusRequest
+    {
+        public int? SupplierId { get; set; }
+        public short? StatusId { get; set; }
+    }
+}
