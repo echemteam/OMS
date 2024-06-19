c70abb9a05c67a8ee804156203b0ff36411e0c25
nisargpatel1 <nisarg.p@mol-tech.com>
2024-06-19 18:47:28 +0530

    tab commant

--- OMS Front End/src/pages/customerDetail/AddCustomer.js
+++ OMS Front End/src/pages/customerDetail/AddCustomer.js
@@ -60,9 +60,9 @@ const AddCustomer = () => {
     },
   ];
 
-  const handleTabClick = (index) => {
-    setActiveTab(index);
-  };
+  // const handleTabClick = (index) => {
+  //   setActiveTab(index);
+  // };
 
   const handleSubmit = () => {
     let req = {
@@ -91,7 +91,7 @@ const AddCustomer = () => {
                 <React.Fragment key={index}>
                   <div className={`step ${activeTab === index ? 'active' : ''}`}>
                     <button className="step-button"
-                      onClick={() => handleTabClick(index)}
+                      // onClick={() => handleTabClick(index)}
                     >
                       <span className="stepper-box">{index + 1}</span>
                       <span className="stepper-label">
