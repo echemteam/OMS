eeca828a91ebaad6f3d516f1cfb7f767740a195e
Amit Sonigara <amit.s@mol-tech.com>
2024-06-21 17:20:13 +0530

    chnage api GetShppingDeliveryCarrierAndDeliveryMethodsById

--- OMS.API/OMS.Domain.Entities/API/Response/CustomerAccountingSettings/GetDeliveryMethodsCustomerIdResponse.cs
+++ OMS.API/OMS.Domain.Entities/API/Response/CustomerAccountingSettings/GetDeliveryMethodsCustomerIdResponse.cs
@@ -5,6 +5,7 @@
         public int? CustomerDeliveryMethodId { get; set; }
         public int? CustomerId { get; set; }
         public byte? DeliveryMethodId { get; set; }
+        public bool? IsForInternational { get; set; }
         public string? Name { get; set; }
         public decimal? Charge { get; set; }
         public bool? IsPrimary { get; set; }
