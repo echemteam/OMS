cc22efe2b8cce5b6746b94d7ff6b418a8bfd1769
Amit Sonigara <amit.s@mol-tech.com>
2024-06-28 17:36:49 +0530

    chnage addedit contact api for for add multiple contact in single time

--- OMS.API/OMS.Application.Services/Contact/ContactService.cs
+++ OMS.API/OMS.Application.Services/Contact/ContactService.cs
@@ -28,85 +28,94 @@ namespace OMS.Application.Services.Contact
         #region  Contact Service
         public async Task<AddEntityDTO<int>> AddEditContact(AddEditContactRequest requestData, short CurrentUserId)
         {
+            string[] contactTypeIds = requestData.ContactTypeId!.Split(',');
+
             AddEntityDTO<int> responceData = new();
-            ContactDTO contactDTO = requestData.ToMapp<AddEditContactRequest, ContactDTO>();
-            contactDTO.CreatedBy = CurrentUserId;
-            responceData = await repositoryManager.contact.AddEditContact(contactDTO);
-
-            if (responceData.KeyValue > 0)
+            foreach (var singleContactTypeId in contactTypeIds)
             {
+                short contactTypeId = short.Parse(singleContactTypeId);
 
-                List<AddContactEmailRequest> emailDT = requestData.EmailList!;
-                List<AddContactPhoneRequest> PhoneDT = requestData.PhoneList!;
+                requestData.ContactTypeId = singleContactTypeId;
+                ContactDTO contactDTO = requestData.ToMapp<AddEditContactRequest, ContactDTO>();
+                contactDTO.CreatedBy = CurrentUserId;
 
-                int contactId = responceData.KeyValue;
-                short ownerTypeId = 0;
+                responceData = await repositoryManager.contact.AddEditContact(contactDTO);
+
+                if (responceData.KeyValue > 0)
+                {
+                    List<AddContactEmailRequest> emailDT = requestData.EmailList!;
+                    List<AddContactPhoneRequest> PhoneDT = requestData.PhoneList!;
+
+                    int contactId = responceData.KeyValue;
+                    short ownerTypeId = 0;
+                    if (requestData.CustomerId > 0 && responceData.KeyValue > 0)
+                    {
+                        ownerTypeId = 3;
+                    }
+                    else if (requestData.SupplierId > 0 && responceData.KeyValue > 0)
+                    {
+                        ownerTypeId = 4;
+                    }
+
+                    if (requestData.EmailList != null && requestData.EmailList.Count > 0)
+                    {
+                        DataTable emailDataTable = ExportHelper.ListToDataTable(emailDT);
+                        emailDataTable.Columns.Add("OwnerTypeId", typeof(short));
+                        emailDataTable.Columns.Add("CreatedBy", typeof(short));
+
+                        foreach (DataRow row in emailDataTable.Rows)
+                        {
+                            row["OwnerTypeId"] = ownerTypeId;
+                            row["CreatedBy"] = CurrentUserId;
+
+                        }
+                        _ = await repositoryManager.emailAddress.AddEditContactEmail(emailDataTable, contactId);
+
+                    }
+                    if (requestData.PhoneList != null && requestData.PhoneList.Count > 0)
+                    {
+                        DataTable phoneDataTable = ExportHelper.ListToDataTable(PhoneDT);
+                        phoneDataTable.Columns.Add("OwnerTypeId", typeof(short));
+                        phoneDataTable.Columns.Add("CreatedBy", typeof(short));
+
+                        foreach (DataRow row in phoneDataTable.Rows)
+                        {
+                            row["OwnerTypeId"] = ownerTypeId;
+                            row["CreatedBy"] = CurrentUserId;
+                        }
+                        _ = await repositoryManager.phoneNumber.AddEditContactPhone(phoneDataTable, contactId);
+                    }
+                }
+
                 if (requestData.CustomerId > 0 && responceData.KeyValue > 0)
                 {
-                    ownerTypeId = 3;
-                }
-                else if (requestData.SupplierId > 0 && responceData.KeyValue > 0)
-                {
-                    ownerTypeId = 4;
-                }
-
-                if (requestData.EmailList != null && requestData.EmailList.Count > 0)
-                {
-                    DataTable emailDataTable = ExportHelper.ListToDataTable(emailDT);
-                    emailDataTable.Columns.Add("OwnerTypeId", typeof(short));
-                    emailDataTable.Columns.Add("CreatedBy", typeof(short));
-               
-                    foreach (DataRow row in emailDataTable.Rows)
+                    AddEditContactForCustomerRequest addEditContactForCustomerRequest = new()
                     {
-                        row["OwnerTypeId"] = ownerTypeId;
-                        row["CreatedBy"] = CurrentUserId;
-                       
-                    }
-                    _ = await repositoryManager.emailAddress.AddEditContactEmail(emailDataTable, contactId);
+                        CustomerContactId = requestData.CustomerContactId,
+                        CustomerId = requestData.CustomerId,
+                        ContactId = responceData.KeyValue,
+                        ContactTypeId = contactTypeId,
+                        IsPrimary = requestData.IsPrimary
+                    };
 
+                    _ = await repositoryManager.customers.AddEditContactForCustomer(addEditContactForCustomerRequest, CurrentUserId);
                 }
-                if (requestData.PhoneList != null && requestData.PhoneList.Count > 0)
+                else if (requestData.SupplierId > 0)
                 {
-                    DataTable phoneDataTable = ExportHelper.ListToDataTable(PhoneDT);
-                    phoneDataTable.Columns.Add("OwnerTypeId", typeof(short));
-                    phoneDataTable.Columns.Add("CreatedBy", typeof(short));
-                 
-                    foreach (DataRow row in phoneDataTable.Rows)
+                    AddEditContactForSupplierRequest addEditContactForSupplierRequest = new()
                     {
-                        row["OwnerTypeId"] = ownerTypeId;
-                        row["CreatedBy"] = CurrentUserId;  
-                    }
-                    _ = await repositoryManager.phoneNumber.AddEditContactPhone(phoneDataTable, contactId);
+                        SupplierContactId = requestData.SupplierContactId,
+                        SupplierId = requestData.SupplierId,
+                        ContactId = responceData.KeyValue,
+                        ContactTypeId = contactTypeId,
+                        IsPrimary = requestData.IsPrimary
+                    };
+                    _ = await repositoryManager.supplier.AddEditContactForSupplier(addEditContactForSupplierRequest, CurrentUserId);
                 }
-            }
-
-            if (requestData.CustomerId > 0 && responceData.KeyValue >0)
-            {
-                AddEditContactForCustomerRequest addEditContactForCustomerRequest = new()
-                {
-                    CustomerContactId = requestData.CustomerContactId,
-                    CustomerId = requestData.CustomerId,
-                    ContactId = responceData.KeyValue,
-                    ContactTypeId = requestData.ContactTypeId,
-                    IsPrimary = requestData.IsPrimary
-                };
-
-                _ = await repositoryManager.customers.AddEditContactForCustomer(addEditContactForCustomerRequest, CurrentUserId);
-            }
-            else if (requestData.SupplierId > 0)
-            {
-                AddEditContactForSupplierRequest addEditContactForSupplierRequest = new()
-                {
-                    SupplierContactId = requestData.SupplierContactId,
-                    SupplierId = requestData.SupplierId,
-                    ContactId = responceData.KeyValue,
-                    ContactTypeId = requestData.ContactTypeId,
-                    IsPrimary = requestData.IsPrimary
-                };
-                _ = await repositoryManager.supplier.AddEditContactForSupplier(addEditContactForSupplierRequest, CurrentUserId);
             }
             return responceData;
         }
+
         public async Task<List<GetContactByCustomerIdResponse>> GetContactByCustomerId(int customerId)
         {
             List<GetContactByCustomerIdResponse> contactList = await repositoryManager.contact.GetContactByCustomerId(customerId);
