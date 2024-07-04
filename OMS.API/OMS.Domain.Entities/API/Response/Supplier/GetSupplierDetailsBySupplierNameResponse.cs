2ba4d79db62fca61bd58616bfda627303f6fa35a
Amit Sonigara <amit.s@mol-tech.com>
2024-07-04 19:07:36 +0530

    add new api for GetSupplierDetailsBySupplierName and GetCustomersDetailsByCutomerName.

---
+++ OMS.API/OMS.Domain.Entities/API/Response/Supplier/GetSupplierDetailsBySupplierNameResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace OMS.Domain.Entities.API.Response.Supplier
+{
+    public class GetSupplierDetailsBySupplierNameResponse
+    {
+        public int? SupplierId { get; set; }
+        public string? Name { get; set; }
+        public string? GroupType { get; set; }
+        public string? CountryName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? TaxId { get; set; }
+        public string? Status { get; set; }
+    }
+}
