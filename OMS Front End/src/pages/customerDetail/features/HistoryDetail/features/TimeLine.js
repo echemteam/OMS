bf75a36848d04c12c38a7334016b3d477de17ad9
kirtan <kirtan.p@mol-tech.com>
2024-07-05 18:30:04 +0530

    update changes

--- OMS Front End/src/pages/customerDetail/features/HistoryDetail/features/TimeLine.js
+++ OMS Front End/src/pages/customerDetail/features/HistoryDetail/features/TimeLine.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from "react";
 import "./../../HistoryDetail/TimeLine.scss";
 import Buttons from "../../../../../components/ui/button/Buttons";
