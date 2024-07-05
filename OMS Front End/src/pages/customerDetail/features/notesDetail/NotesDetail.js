049446e807e8de090640454c6e3624a5eee219f0
nisargpatel1 <nisarg.p@mol-tech.com>
2024-07-05 18:26:31 +0530

    Remove warning

--- OMS Front End/src/pages/customerDetail/features/notesDetail/NotesDetail.js
+++ OMS Front End/src/pages/customerDetail/features/notesDetail/NotesDetail.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useRef, useState } from "react";
 import CardSection from "../../../../components/ui/card/CardSection";
 import { AppIcons } from "../../../../data/appIcons";
