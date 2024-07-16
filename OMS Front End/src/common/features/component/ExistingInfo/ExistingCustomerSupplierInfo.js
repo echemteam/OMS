f506c116da594feaf48f0b5b56f212335709f86e
shrutenparmar <shruten.p@mol-tech.com>
2024-07-16 11:34:50 +0530

    CHANGES.

--- OMS Front End/src/common/features/component/ExistingInfo/ExistingCustomerSupplierInfo.js
+++ OMS Front End/src/common/features/component/ExistingInfo/ExistingCustomerSupplierInfo.js
@@ -7,6 +7,7 @@ import SidebarModel from '../../../../components/ui/sidebarModel/SidebarModel';
 //** Service's */
 import { encryptUrlData } from '../../../../services/CryptoService';
 import ToastService from '../../../../services/toastService/ToastService';
+import { ErrorMessage } from '../../../../data/appMessages';
 
 const ExistingCustomerSupplierInfo = forwardRef(({ parentRef, isSupplier, getExistingInfoByName }) => {
 
@@ -26,7 +27,7 @@ const ExistingCustomerSupplierInfo = forwardRef(({ parentRef, isSupplier, getExi
                 setIsExistingModel(true);
                 setExistingInfoData(isGetSupplierDetailsBySupplierNameData)
             } else {
-                ToastService.warning("No record found");
+                ToastService.info(ErrorMessage.NoFound);
             }
         }
     }, [isGetSupplierDetailsBySupplierNameFetching, isGetSupplierDetailsBySupplierNameSucess, isGetSupplierDetailsBySupplierNameData]);
