4c3797db9bb1c52c3aaa18a59a9cee1caac90325
shrutenparmar <shruten.p@mol-tech.com>
2024-07-18 19:48:30 +0530

    changes.

--- OMS Front End/src/common/features/component/SubCustomer/feature/AddEditSubCustomer.js
+++ OMS Front End/src/common/features/component/SubCustomer/feature/AddEditSubCustomer.js
@@ -10,68 +10,68 @@ import { useEffect } from "react";
 import ToastService from "../../../../../services/toastService/ToastService";
 import { onResetForm } from "../../../../../utils/FormFields/ResetForm/handleResetForm";
 
-const AddEditSubCustomer=(props)=>{
+const AddEditSubCustomer = (props) => {
 
-    const subcustomerRef=useRef();
-    const [subCustomerFormData, setSubCustomerFormData] = useState(SubCustomerFormData);
-    const [shouldRerenderFormCreator, setShouldRerenderFormCreator] = useState(false);
+  const subcustomerRef = useRef();
+  const [subCustomerFormData, setSubCustomerFormData] = useState(SubCustomerFormData);
+  const [shouldRerenderFormCreator, setShouldRerenderFormCreator] = useState(false);
 
-    const [getAllSubCompany, { isFetching: isGetAllSubCompanyFetching, isSuccess: isGetAllSubCompanySuccess, data: isGetAllSubCompanyData }] = useLazyGetAllSubCompanyQuery();
-    const [addSubCompanyMainCompany, { isLoading: isAddSubCompanyMainCompanyLoading, isSuccess: isAddSubCompanyMainCompanySuccess, data: isAddSubCompanyMainCompanyData }] = useAddSubCompanyMainCompanyMutation();
-    useEffect(() => {
-      if (!isGetAllSubCompanyFetching && isGetAllSubCompanySuccess && isGetAllSubCompanyData) {
-        
-        setDropDownOptionField(isGetAllSubCompanyData, 'customerId', 'name', subCustomerFormData, 'customerId');
-        setShouldRerenderFormCreator((prevState) => !prevState);
-      }
-    }, [isGetAllSubCompanyFetching && isGetAllSubCompanySuccess, isGetAllSubCompanyData]);
+  const [getAllSubCompany, { isFetching: isGetAllSubCompanyFetching, isSuccess: isGetAllSubCompanySuccess, data: isGetAllSubCompanyData }] = useLazyGetAllSubCompanyQuery();
+  const [addSubCompanyMainCompany, { isLoading: isAddSubCompanyMainCompanyLoading, isSuccess: isAddSubCompanyMainCompanySuccess, data: isAddSubCompanyMainCompanyData }] = useAddSubCompanyMainCompanyMutation();
+  useEffect(() => {
+    if (!isGetAllSubCompanyFetching && isGetAllSubCompanySuccess && isGetAllSubCompanyData) {
 
-    useEffect(()=>{
-      getAllSubCompany(props?.isSubCompany);
-    },[])
-  
-    useEffect(() => {
-      if (isAddSubCompanyMainCompanySuccess && isAddSubCompanyMainCompanyData) {
-       props.onSuccess()
-       
-        ToastService.success(isAddSubCompanyMainCompanyData.errorMessage);
-        onResetForm(SubCustomerFormData, setSubCustomerFormData, null);
-      }
-    }, [isAddSubCompanyMainCompanySuccess, isAddSubCompanyMainCompanyData]);
-  
-const onhandleEditSubcustomer=()=>{
-  const formData=subcustomerRef.current.getFormData();
- 
-  if(formData && !formData.subCompanyMainCompanyId){
-    const customerIdList = formData.customerId.map((item) => ({ customerId: item }));
-    let customerId = customerIdList.map((item) => item.customerId).join(",");
-    const request={
-    ...formData,
-    mainCompanyId:props.customerId,
-    subCompanyId:customerId,
+      setDropDownOptionField(isGetAllSubCompanyData, 'customerId', 'name', subCustomerFormData, 'customerId');
+      setShouldRerenderFormCreator((prevState) => !prevState);
     }
-    addSubCompanyMainCompany(request);
-  }
-}
+  }, [isGetAllSubCompanyFetching && isGetAllSubCompanySuccess, isGetAllSubCompanyData]);
 
-    return(<>
-        <div className="row">
-    
-        <FormCreator
-         config={subCustomerFormData}
-          ref={subcustomerRef}
-          key={shouldRerenderFormCreator}
-         {...subCustomerFormData}
-         />
-            <div className="col-xxl-6 col-xl-12 col-md-12 col-12 col-12 mt-4">
-              <Buttons
-                buttonTypeClassName="theme-button"
-                buttonText="Assign"
-                onClick={onhandleEditSubcustomer}
-                isLoading={isAddSubCompanyMainCompanyLoading}
-              />
-            </div>
+  useEffect(() => {
+    getAllSubCompany(props?.isSubCompany ? !props?.isSubCompany : true);
+  }, [props?.isSubCompany])
+
+  useEffect(() => {
+    if (isAddSubCompanyMainCompanySuccess && isAddSubCompanyMainCompanyData) {
+      props.onSuccess()
+
+      ToastService.success(isAddSubCompanyMainCompanyData.errorMessage);
+      onResetForm(SubCustomerFormData, setSubCustomerFormData, null);
+    }
+  }, [isAddSubCompanyMainCompanySuccess, isAddSubCompanyMainCompanyData]);
+
+  const onhandleEditSubcustomer = () => {
+    const formData = subcustomerRef.current.getFormData();
+
+    if (formData && !formData.subCompanyMainCompanyId) {
+      const customerIdList = formData.customerId.map((item) => ({ customerId: item }));
+      let customerId = customerIdList.map((item) => item.customerId).join(",");
+      const request = {
+        ...formData,
+        mainCompanyId: props.customerId,
+        subCompanyId: customerId,
+      }
+      addSubCompanyMainCompany(request);
+    }
+  }
+
+  return (<>
+    <div className="row">
+
+      <FormCreator
+        config={subCustomerFormData}
+        ref={subcustomerRef}
+        key={shouldRerenderFormCreator}
+        {...subCustomerFormData}
+      />
+      <div className="col-xxl-6 col-xl-12 col-md-12 col-12 col-12 mt-4">
+        <Buttons
+          buttonTypeClassName="theme-button"
+          buttonText="Assign"
+          onClick={onhandleEditSubcustomer}
+          isLoading={isAddSubCompanyMainCompanyLoading}
+        />
+      </div>
     </div>
-    </>)
-   }
-   export default AddEditSubCustomer;
+  </>)
+}
+export default AddEditSubCustomer;
