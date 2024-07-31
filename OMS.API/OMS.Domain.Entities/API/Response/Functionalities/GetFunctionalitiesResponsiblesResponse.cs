099a21d95d0cdb7ac6f6a298e59fbb21770e6851
Amit Sonigara <amit.s@mol-tech.com>
2024-07-31 16:53:06 +0530

    change api responce

--- OMS.API/OMS.Domain.Entities/API/Response/Functionalities/GetFunctionalitiesResponsiblesResponse.cs
+++ OMS.API/OMS.Domain.Entities/API/Response/Functionalities/GetFunctionalitiesResponsiblesResponse.cs
@@ -2,5 +2,11 @@
 {
     public class GetFunctionalitiesResponsiblesResponse
     {
+        public int? FunctionalitiesResponsiblesId { get; set; }
+        public int? FunctionalityId { get; set; }
+        public string? FunctionalityName { get; set; }
+        public short? ResponsibleUserId { get; set; }
+        public string? ResponsibleUserName { get; set; }
+
     }
 }
