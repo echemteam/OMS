ca613ed266b151e4095835a9a06ac5c2bcdce49c
JanviMoltech <janavi.moltech@gmail.com>
2024-07-31 11:10:19 +0530

    Update changes

--- OMS Front End/src/pages/organization/feature/organizationContactDetail/OrganizationContactDetail.js
+++ OMS Front End/src/pages/organization/feature/organizationContactDetail/OrganizationContactDetail.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useRef,useState,useEffect } from "react";
 import FormCreator from "../../../../components/Forms/FormCreator";
 import { OrganizationContactFormData } from "./config/OrganizationContact.data";
