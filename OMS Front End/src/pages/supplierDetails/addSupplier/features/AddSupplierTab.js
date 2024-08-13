480816373a2538a2b99d2c36ea925e9e530aee36
nisargpatel1 <nisarg.p@mol-tech.com>
2024-08-13 18:49:21 +0530

    remove code

--- OMS Front End/src/pages/supplierDetails/addSupplier/features/AddSupplierTab.js
+++ OMS Front End/src/pages/supplierDetails/addSupplier/features/AddSupplierTab.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import CardSection from "../../../../components/ui/card/CardSection";
