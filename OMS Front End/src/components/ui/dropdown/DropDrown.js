b550411b48b51c5b412d07210241dbb4e46be845
nisargpatel1 <nisarg.p@mol-tech.com>
2024-08-14 18:07:26 +0530

    Changes

--- OMS Front End/src/components/ui/dropdown/DropDrown.js
+++ OMS Front End/src/components/ui/dropdown/DropDrown.js
@@ -13,8 +13,6 @@ const DropDown = (props) => {
   const defaultSelectedOptionMultiSelect = Array.isArray(props.value) ?
     props.options.filter(option => props.value.includes(option.value)) : [];
 
-    console.log("props" , props)
-    console.log("defaultSelectedOptionMultiSelect" , defaultSelectedOptionMultiSelect)
   return (
     <>
       <span className="d-inline-block custom-input">
