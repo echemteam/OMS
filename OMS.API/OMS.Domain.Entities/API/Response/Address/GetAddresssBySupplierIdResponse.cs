2d706fcfe3068659f8b482f75a63a87d524190df
Amit Sonigara <amit.s@mol-tech.com>
2024-08-14 14:42:24 +0530

    chnage api

--- OMS.API/OMS.Domain.Entities/API/Response/Address/GetAddresssBySupplierIdResponse.cs
+++ OMS.API/OMS.Domain.Entities/API/Response/Address/GetAddresssBySupplierIdResponse.cs
@@ -15,6 +15,7 @@
         public string? CountryName { get; set; }
         public int? StateId { get; set; }
         public string? StateName { get; set; }
+        public string? StateCode { get; set; }
         public int? CityId { get; set; }
         public string? CityName { get; set; }
         public int? ZipCode { get; set; }
