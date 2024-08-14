0a8d15f6272ecc4201865e196e8fab6e4d3e858a
VaibhavWaghela <vaibhav.w@mol-tech.com>
2024-08-14 11:09:40 +0530

    GetAllAccountType new method create

---
+++ OMS.API/ClientIPAuthentication/Enum/GetAllAccountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientIPAuthentication.Enum
+{
+    public enum GetAllAccountType
+    {
+        checking = 1,
+        saving = 2
+    }
+}
