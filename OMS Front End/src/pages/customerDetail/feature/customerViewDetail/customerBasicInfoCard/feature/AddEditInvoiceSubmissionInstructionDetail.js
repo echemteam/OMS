9ceb160dcdacbd5845d212e105a00eb53a94df32
kirtan <kirtan.p@mol-tech.com>
2024-08-14 19:14:33 +0530

    update changes

--- OMS Front End/src/pages/customerDetail/feature/customerViewDetail/customerBasicInfoCard/feature/AddEditInvoiceSubmissionInstructionDetail.js
+++ OMS Front End/src/pages/customerDetail/feature/customerViewDetail/customerBasicInfoCard/feature/AddEditInvoiceSubmissionInstructionDetail.js
@@ -5,71 +5,70 @@ import { InvoiceFormData } from "../../../../../supplierDetails/feature/supplier
 import { useAddEditCustomerInvoiceMutation, useLazyGetDetailsbyCustomerIDQuery } from "../../../../../../app/services/customerSettingsAPI";
 import ToastService from "../../../../../../services/toastService/ToastService";
 
-const AddEditInvoiceSubmissionInstructionDetail=({customerId,handleToggleModal,showModal,setShowModal})=>{
+const AddEditInvoiceSubmissionInstructionDetail = ({ customerId, handleToggleModal, showModal, setShowModal }) => {
     const invoiceRef = useRef();
     const [formData, setFormData] = useState(InvoiceFormData);
     const [shouldRerenderFormCreator, setShouldRerenderFormCreator] = useState(false);
     const [addEditCustomerInvoice, { isLoading: isAddEditCustomerInvoiceLoading, isSuccess: isAddEditCustomerInvoiceSuccess, data: isAddEditCustomerInvoiceData }] = useAddEditCustomerInvoiceMutation();
     const [GetDetailsbyCustomerID, { isFetching: isGetDetailByCustomerIdFetching, isSuccess: isGetDetailByCustomerIdSuccess, data: isGetDetailByCustomerIdData, },] = useLazyGetDetailsbyCustomerIDQuery();
 
-    useEffect(()=>{
-    if(showModal){
-         GetDetailsbyCustomerID(customerId);
-    }
-   },[showModal])
+    useEffect(() => {
+        if (showModal) {
+            GetDetailsbyCustomerID(customerId);
+        }
+    }, [showModal])
 
-   useEffect(()=>{
-    if (!isGetDetailByCustomerIdFetching && isGetDetailByCustomerIdSuccess && isGetDetailByCustomerIdData) {
-        debugger
-        let newFormData = { ...formData };
-        newFormData.initialState = {
-          invoiceSubmissionInstruction:isGetDetailByCustomerIdData.invoiceSubmissionInstruction,
-        }; 
-        setFormData(newFormData);
-    }
-   },[isGetDetailByCustomerIdFetching && isGetDetailByCustomerIdSuccess && isGetDetailByCustomerIdData])
-   
- useEffect(()=>{
-    if(isAddEditCustomerInvoiceSuccess && isAddEditCustomerInvoiceData){
-         ToastService.success(isAddEditCustomerInvoiceData.errorMessage);       
-        setShowModal(false);
-}
-}, [isAddEditCustomerInvoiceSuccess, isAddEditCustomerInvoiceData]);
+    useEffect(() => {
+        if (!isGetDetailByCustomerIdFetching && isGetDetailByCustomerIdSuccess && isGetDetailByCustomerIdData) {
+            let newFormData = { ...formData };
+            newFormData.initialState = {
+                invoiceSubmissionInstruction: isGetDetailByCustomerIdData.invoiceSubmissionInstruction,
+            };
+            setFormData(newFormData);
+        }
+    }, [isGetDetailByCustomerIdFetching, isGetDetailByCustomerIdSuccess, isGetDetailByCustomerIdData])
 
-    const handleInvoiceSubmissionData=()=>{
+    useEffect(() => {
+        if (isAddEditCustomerInvoiceSuccess && isAddEditCustomerInvoiceData) {
+            ToastService.success(isAddEditCustomerInvoiceData.errorMessage);
+            setShowModal(false);
+        }
+    }, [isAddEditCustomerInvoiceSuccess, isAddEditCustomerInvoiceData]);
+
+    const handleInvoiceSubmissionData = () => {
         let invoiceData = invoiceRef.current.getFormData();
         let request = {
-        customerId:customerId,
-        invoiceSubmissionInstruction:invoiceData.invoiceSubmissionInstruction,
-        customerAccountingSettingId: isGetDetailByCustomerIdData?.customerAccountingSettingId || 0,
+            customerId: customerId,
+            invoiceSubmissionInstruction: invoiceData.invoiceSubmissionInstruction,
+            customerAccountingSettingId: isGetDetailByCustomerIdData?.customerAccountingSettingId || 0,
         };
 
         if (invoiceData && !invoiceData.customerAccountingSettingId) {
             addEditCustomerInvoice(request);
-        }else{
+        } else {
             addEditCustomerInvoice(request);
         }
     }
 
-    return( <div className="row custom-height-tiny add-edit-notesForm">
-    <FormCreator config={formData} ref={invoiceRef} {...formData} key={shouldRerenderFormCreator} />
-    <div className="col-md-12">
-        <div className="d-flex align-item-end justify-content-end">
-            <div className="d-flex align-item-end">
-                <Buttons
-                    buttonTypeClassName="theme-button"
-                    buttonText="Save"
-                     onClick={handleInvoiceSubmissionData}
-                    isLoading={isAddEditCustomerInvoiceLoading }
+    return (<div className="row custom-height-tiny add-edit-notesForm">
+        <FormCreator config={formData} ref={invoiceRef} {...formData} key={shouldRerenderFormCreator} />
+        <div className="col-md-12">
+            <div className="d-flex align-item-end justify-content-end">
+                <div className="d-flex align-item-end">
+                    <Buttons
+                        buttonTypeClassName="theme-button"
+                        buttonText="Save"
+                        onClick={handleInvoiceSubmissionData}
+                        isLoading={isAddEditCustomerInvoiceLoading}
                     />
-                <Buttons
-                    buttonTypeClassName="dark-btn ml-5"
-                    buttonText="Cancel"
-                    onClick={handleToggleModal}
+                    <Buttons
+                        buttonTypeClassName="dark-btn ml-5"
+                        buttonText="Cancel"
+                        onClick={handleToggleModal}
                     />
+                </div>
             </div>
         </div>
-    </div>
-</div>)
+    </div>)
 }
 export default AddEditInvoiceSubmissionInstructionDetail;
