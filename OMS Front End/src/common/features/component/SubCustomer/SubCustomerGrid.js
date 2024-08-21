38658f10e5fa061dbfc368abde5aba28f18a760e
Praful Desai <praful.d@mol-tech.com>
2024-08-21 16:45:48 +0530

    Minor UI Bug fixed

--- OMS Front End/src/common/features/component/SubCustomer/SubCustomerGrid.js
+++ OMS Front End/src/common/features/component/SubCustomer/SubCustomerGrid.js
@@ -5,31 +5,40 @@ import SubCustomerList from "./feature/SubCustomerList";
 import PropTypes from "prop-types";
 
 const SubCustomerGrid = ({ customerId }) => {
-    const childRef = useRef();
-    const getLinkCustomerRef = useRef();
+  const childRef = useRef();
+  const getLinkCustomerRef = useRef();
 
-    const onSuccess = () => {
-        if (childRef.current) {
-            childRef.current.callChildFunction();
-        }
-    };
+  const onSuccess = () => {
+    if (childRef.current) {
+      childRef.current.callChildFunction();
+    }
+  };
 
-    const onGetLinkCustomer = () => {
-        if (getLinkCustomerRef.current) {
-            getLinkCustomerRef.current.callChildFunction();
-        }
-    };
+  const onGetLinkCustomer = () => {
+    if (getLinkCustomerRef.current) {
+      getLinkCustomerRef.current.callChildFunction();
+    }
+  };
 
-    return (
-
-        <CardSection cardTitle="Link Customer" buttonClassName="theme-button">
-            <AddEditSubCustomer customerId={customerId} onSuccess={onSuccess} getLinkCustomerRef={getLinkCustomerRef} />
-            <SubCustomerList customerId={customerId} childRef={childRef} onGetLinkCustomer={onGetLinkCustomer} />
-        </CardSection>
-    )
-}
+  return (
+    <div className="link-customer">
+      <CardSection cardTitle="Link Customer" buttonClassName="theme-button">
+        <AddEditSubCustomer
+          customerId={customerId}
+          onSuccess={onSuccess}
+          getLinkCustomerRef={getLinkCustomerRef}
+        />
+        <SubCustomerList
+          customerId={customerId}
+          childRef={childRef}
+          onGetLinkCustomer={onGetLinkCustomer}
+        />
+      </CardSection>
+    </div>
+  );
+};
 
 SubCustomerGrid.propTypes = {
-    customerId: PropTypes.number.isRequired,  
+  customerId: PropTypes.number.isRequired,
 };
 export default SubCustomerGrid;
