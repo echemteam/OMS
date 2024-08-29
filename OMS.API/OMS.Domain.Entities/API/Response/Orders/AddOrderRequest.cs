967e1d10b9d8a413960d27b718cd7e32c55c9086
Amit Sonigara <amit.s@mol-tech.com>
2024-08-29 16:14:36 +0530

    API: Changes in the add order API when tables will be finalized

--- OMS.API/OMS.Domain.Entities/API/Response/Orders/AddOrderRequest.cs
+++ OMS.API/OMS.Domain.Entities/API/Response/Orders/AddOrderRequest.cs
@@ -5,15 +5,6 @@ namespace OMS.Domain.Entities.API.Response.Orders
 {
     public class AddOrderRequest
     {
-        //public int? OrderId { get; set; }
-        //public byte? OrderMethodId { get; set; }
-        //public int? CustomerId { get; set; }
-        //public int? SubCustomerId { get; set; }
-        //public string? PoNumber { get; set; }
-        //public DateTime? OrderReceivedDate { get; set; }
-        //public int? OrderAddressId { get; set; }
-        //public int? BillingAddressId { get; set; }
-        //public int? ShippingAddressId { get; set; }
         public int? OrderId { get; set; }
         public byte? OrderMethodId { get; set; }
         public int? CustomerId { get; set; }
