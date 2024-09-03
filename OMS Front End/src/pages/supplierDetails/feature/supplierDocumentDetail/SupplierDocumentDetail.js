cd561c530205b05658ee3f286f83504c7e30dde3
pankajchauhan <pankaj.c@mol-tech.com>
2024-09-03 09:59:42 +0530

    adjust the UI Design

--- OMS Front End/src/pages/supplierDetails/feature/supplierDocumentDetail/SupplierDocumentDetail.js
+++ OMS Front End/src/pages/supplierDetails/feature/supplierDocumentDetail/SupplierDocumentDetail.js
@@ -1,38 +1,50 @@
-import React, { useContext } from 'react';
-import { securityKey } from '../../../../data/SecurityKey';
-import AddSupplierContext from '../../../../utils/ContextAPIs/Supplier/AddSupplierContext';
-import PropTypes from 'prop-types';
+import React, { useContext } from "react";
+import { securityKey } from "../../../../data/SecurityKey";
+import AddSupplierContext from "../../../../utils/ContextAPIs/Supplier/AddSupplierContext";
+import PropTypes from "prop-types";
 //** Service */
-import { useLazyDownloadDocumentQuery } from '../../../../app/services/documentAPI';
-import { useAddSupplierDocumentsMutation, useDeleteSupplierDocumentsByIdMutation, useLazyGetSupplierDocumentsByIdQuery } from '../../../../app/services/supplierDocuementsAPI';
+import { useLazyDownloadDocumentQuery } from "../../../../app/services/documentAPI";
+import {
+  useAddSupplierDocumentsMutation,
+  useDeleteSupplierDocumentsByIdMutation,
+  useLazyGetSupplierDocumentsByIdQuery,
+} from "../../../../app/services/supplierDocuementsAPI";
 //** Component's */
-const DocumentGrid = React.lazy(() => import("../../../../common/features/component/Document/DocumentGrid"));
-
+const DocumentGrid = React.lazy(() =>
+  import("../../../../common/features/component/Document/DocumentGrid")
+);
 
 const supplierSecurityKey = {
-    ADD: securityKey.ADDSUPPLIERDOCUMENT,
-    DELETE: securityKey.DELETESUPPLIERDOCUMENT,
-    DOWNALOD: securityKey.DOWNALODSUPPLIERDOCUMENT,
-}
+  ADD: securityKey.ADDSUPPLIERDOCUMENT,
+  DELETE: securityKey.DELETESUPPLIERDOCUMENT,
+  DOWNALOD: securityKey.DOWNALODSUPPLIERDOCUMENT,
+};
 
 const SupplierDocumentDetail = ({ isEditablePage }) => {
+  const { supplierId, isResponsibleUser } = useContext(AddSupplierContext);
 
-    const { supplierId, isResponsibleUser } = useContext(AddSupplierContext);
-
-    return (
-        /**
-            * This component displays an DocumentGrid for the supplier module.
-            * This hook dynamically sets the API call based on the module (customer or supplier).
-        */
-        <DocumentGrid keyId={supplierId || 0} isSupplier={true} isEditablePage={isEditablePage}
-            SecurityKey={!isResponsibleUser ? supplierSecurityKey : null}
-            addDocuments={useAddSupplierDocumentsMutation} downloadDocument={useLazyDownloadDocumentQuery}
-            deleteDocumentsById={useDeleteSupplierDocumentsByIdMutation} getDocumentsById={useLazyGetSupplierDocumentsByIdQuery} />
-    )
-}
+  return (
+    /**
+     * This component displays an DocumentGrid for the supplier module.
+     * This hook dynamically sets the API call based on the module (customer or supplier).
+     */
+    <div className="supplier-document">
+      <DocumentGrid
+        keyId={supplierId || 0}
+        isSupplier={true}
+        isEditablePage={isEditablePage}
+        SecurityKey={!isResponsibleUser ? supplierSecurityKey : null}
+        addDocuments={useAddSupplierDocumentsMutation}
+        downloadDocument={useLazyDownloadDocumentQuery}
+        deleteDocumentsById={useDeleteSupplierDocumentsByIdMutation}
+        getDocumentsById={useLazyGetSupplierDocumentsByIdQuery}
+      />
+    </div>
+  );
+};
 
 SupplierDocumentDetail.propTypes = {
-    isEditablePage: PropTypes.bool.isRequired,
+  isEditablePage: PropTypes.bool.isRequired,
 };
 
 export default SupplierDocumentDetail;
