e5156fec96de53c630e4be372e42649419e9f91f
Mahesh Bariya <mahesh.b@mol-tech.com>
2024-09-03 17:31:37 +0530

    Responsive order and Configuration Page

--- OMS Front End/src/pages/order/feature/orderItemDetail/OrderItemDetail.js
+++ OMS Front End/src/pages/order/feature/orderItemDetail/OrderItemDetail.js
@@ -99,7 +99,7 @@ const OrderItemDetail = ({ onhandleAddOrderData }) => {
   return (
     <div className="row">
       <ProductDetailsList onhandleProductDetailsListData={handleProductDetailsListData} isDocumentData={isDocumentData}/>
-      <div className="col-xl-6 col-2xl-6 col-lg-12 col-12">
+      <div className="col-xl-6 col-2xl-6 col-lg-12 col-12 mb-2">
         <VerifyProductDetail productId={productId} onVerifyProductList={handleVerifyProductDetail} />
       </div>
       <div className="col-12">
