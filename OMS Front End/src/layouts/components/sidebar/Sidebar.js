9150490bf48c29abc17cc78364ea3eebe5724e5c
pankajchauhan <pankaj.c@mol-tech.com>
2024-09-18 14:25:44 +0530

    adjust the UI design

--- OMS Front End/src/layouts/components/sidebar/Sidebar.js
+++ OMS Front End/src/layouts/components/sidebar/Sidebar.js
@@ -107,7 +107,8 @@ const Sidebar = () => {
                       */}
                       <li className="menu-item mt-4" onClick={handleBackButtonClick}>
                         <Link>
-                          <Image imagePath={AppIcons.BackArrowIcon} altText="button Icon" imgCustomClassName="sidebar-backIcon" />
+                          {/* <Image imagePath={AppIcons.BackArrowIcon} altText="button Icon" imgCustomClassName="sidebar-backIcon" /> */}
+                          <Iconify icon="lets-icons:back" />
                           <span>Back to Main Menu</span>
                         </Link>
                       </li>
