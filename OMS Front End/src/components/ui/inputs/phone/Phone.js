03e0bd3570166a5e0aab2d72996fe37c4c6352ce
kirtan <kirtan.p@mol-tech.com>
2024-09-25 19:25:57 +0530

    update changes css

--- OMS Front End/src/components/ui/inputs/phone/Phone.js
+++ OMS Front End/src/components/ui/inputs/phone/Phone.js
@@ -37,7 +37,7 @@ const Phone = ({
         disabled={isDisable}
         placeholder={placeholder}
         {...inputAttributes}
-        className="input-group"
+        className="input-group custom-phone"
       />
     </div>
   );
