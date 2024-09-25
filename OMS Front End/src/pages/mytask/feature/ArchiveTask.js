6086eb357e4f163fa34b2f96ec162a3392e89339
pankajchauhan <pankaj.c@mol-tech.com>
2024-09-25 17:56:59 +0530

    adjust the Ui Design

--- OMS Front End/src/pages/mytask/feature/ArchiveTask.js
+++ OMS Front End/src/pages/mytask/feature/ArchiveTask.js
@@ -16,16 +16,24 @@ import ModuleList from "./ModuleList";
 import { MyTaskStatus } from "../../../utils/Enums/commonEnums";
 
 const ArchiveTask = (props) => {
-
   const authData = getAuthProps();
   const roleId = authData.roles.roleId;
   const [orderBy, setOrderBy] = useState("Newest");
   const [activeTab, setActiveTab] = useState(null);
   const [archiveData, setArchiveData] = useState([]);
   const [selectedfilterBy, setSelectedFilterBy] = useState([]);
-  const [selectedModule, setSelectedModule] = useState(props.moduleList[0]?.moduleId);
+  const [selectedModule, setSelectedModule] = useState(
+    props.moduleList[0]?.moduleId
+  );
 
-  const [getApprovalRequestsListByStatus, { isLoading, isSuccess: isGetApprovalRequestsListByStatusSuccess, data: isGetApprovalRequestsListByStatusData }] = useGetApprovalRequestsListByStatusAndRoleIdMutation();
+  const [
+    getApprovalRequestsListByStatus,
+    {
+      isLoading,
+      isSuccess: isGetApprovalRequestsListByStatusSuccess,
+      data: isGetApprovalRequestsListByStatusData,
+    },
+  ] = useGetApprovalRequestsListByStatusAndRoleIdMutation();
 
   useEffect(() => {
     if (roleId && props.moduleList && props.moduleList.length > 0) {
@@ -34,24 +42,37 @@ const ArchiveTask = (props) => {
   }, [roleId, props.moduleList]);
 
   useEffect(() => {
-    if (isGetApprovalRequestsListByStatusSuccess && isGetApprovalRequestsListByStatusData) {
+    if (
+      isGetApprovalRequestsListByStatusSuccess &&
+      isGetApprovalRequestsListByStatusData
+    ) {
       setArchiveData(isGetApprovalRequestsListByStatusData);
-      if (!isGetApprovalRequestsListByStatusData || isGetApprovalRequestsListByStatusData?.length === 0) {
+      if (
+        !isGetApprovalRequestsListByStatusData ||
+        isGetApprovalRequestsListByStatusData?.length === 0
+      ) {
         if (props.handleRestEventDetail) {
           props.handleRestEventDetail();
         }
       } else {
-        handleTabClick(isGetApprovalRequestsListByStatusData[0]?.approvalRequestId);
+        handleTabClick(
+          isGetApprovalRequestsListByStatusData[0]?.approvalRequestId
+        );
       }
     }
-  }, [isGetApprovalRequestsListByStatusSuccess, isGetApprovalRequestsListByStatusData]);
+  }, [
+    isGetApprovalRequestsListByStatusSuccess,
+    isGetApprovalRequestsListByStatusData,
+  ]);
 
   const handleRequest = (key, value) => {
     const request = {
-      status: [MyTaskStatus.Accept, MyTaskStatus.Reject].join(','),
+      status: [MyTaskStatus.Accept, MyTaskStatus.Reject].join(","),
       roleId: props.roleId,
       sortOrder: orderBy || "Newest",
-      eventIds: Array.isArray(selectedfilterBy) ? selectedfilterBy.map(String).join(",") : selectedfilterBy,
+      eventIds: Array.isArray(selectedfilterBy)
+        ? selectedfilterBy.map(String).join(",")
+        : selectedfilterBy,
       moduleId: selectedModule || props.moduleList[0]?.moduleId,
     };
 
@@ -90,16 +111,21 @@ const ArchiveTask = (props) => {
   return (
     <>
       <div className="row">
-        <div className="col-5 pr-0">
-          <ModuleList moduleList={props.moduleList}
+        <div className="col-5 pr-0 left-modual-sec">
+          <ModuleList
+            moduleList={props.moduleList}
             onModuleChange={handleModuleClick}
           />
         </div>
         <div className="col-7 pl-1 pr-1">
-          <CardSection cardTitle="Events" rightButton={true} isShort={true}
+          <CardSection
+            cardTitle="Events"
+            rightButton={true}
+            isShort={true}
             filtersOptions={props.eventList}
             selectedFilterOptions={selectedFilterOptions}
-            selectedSortOrder={selectedSortOrder}>
+            selectedSortOrder={selectedSortOrder}
+          >
             <div className="customer-info">
               {isLoading ? (
                 <DataLoader />
@@ -109,45 +135,49 @@ const ArchiveTask = (props) => {
                     archiveData.map((tab) => (
                       <button
                         key={tab.approvalRequestId} // Use a unique key
-                        className={`tab-button ${activeTab === tab.approvalRequestId ? "active" : ""
-                          }`}
+                        className={`tab-button ${
+                          activeTab === tab.approvalRequestId ? "active" : ""
+                        }`}
                         onClick={() => handleTabClick(tab.approvalRequestId)}
                       >
-                        <div className="d-flex align-items-center">
+                        <div className="d-flex align-items-start">
                           <span className="profile-icon">
                             {" "}
                             {FirstSecondLetter(tab.eventName)}{" "}
                           </span>
-                          {/* <div className="title">
-                                        {tab.functionalityName}
-                                        <span className="sub-title">{tab.moduleName}</span>
-                                    </div> */}
-                          <div className="title">
-                            {tab.eventName}
+                          <div>
+                            <div className="title">
+                              {tab.eventName}
+                              <div className="date">
+                                {tab.requestedDate
+                                  ? formatDate(
+                                      tab.requestedDate,
+                                      "MM/DD/YYYY hh:mm A"
+                                    )
+                                  : "No Date"}
+                              </div>
+                            </div>
                             <div className="bage-fix">
                               {/* <span className="sub-title">
                                 {tab.moduleName}
                               </span> */}
                               <div
-                                className={`mytask-type-badge ${tab.status === "Accept"
-                                  ? "badge-accept"
-                                  : tab.status === "Reject"
+                                className={`mytask-type-badge ${
+                                  tab.status === "Accept"
+                                    ? "badge-accept"
+                                    : tab.status === "Reject"
                                     ? "badge-reject"
                                     : ""
-                                  }`}
+                                }`}
                               >
-                               {tab.status} by {tab.approvedByUserName} on {formatDate(tab.approvedDate,"MM/DD/YYYY hh:mm A")}
+                                {tab.status} by {tab.approvedByUserName} on{" "}
+                                {formatDate(
+                                  tab.approvedDate,
+                                  "MM/DD/YYYY hh:mm A"
+                                )}
                               </div>
                             </div>
                           </div>
-                        </div>
-                        <div className="date">
-                          {tab.requestedDate
-                            ? formatDate(
-                              tab.requestedDate,
-                              "MM/DD/YYYY hh:mm A"
-                            )
-                            : "No Date"}
                         </div>
                       </button>
                     ))
