2dff86bd773684f8555786556df219ac65039c01
JanviMoltech <janavi.moltech@gmail.com>
2024-09-26 18:47:15 +0530

    My task in count changes

--- OMS Front End/src/pages/mytask/feature/ModuleList.js
+++ OMS Front End/src/pages/mytask/feature/ModuleList.js
@@ -2,11 +2,28 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import CardSection from "../../../components/ui/card/CardSection";
+import { useLazyGetAllModulesWithPendingRequestCountQuery } from "../../../app/services/commonAPI";
 
-const ModuleList = ({ moduleList, onModuleChange ,isPending}) => {
+const ModuleList = ({ moduleList, onModuleChange ,isPending,setModuleList}) => {
     const [activeModule, setActiveModule] = useState(null);
+    const [
+        getAllModulesWithPendingRequestCount,
+        { isSuccess: isGetAllModulesWithPendingRequestCountSucess, data: allGetAllModulesWithPendingRequestCountData },
+      ] = useLazyGetAllModulesWithPendingRequestCountQuery();
 
     // Set the first module as active by default when the component mounts
+    useEffect(() => {
+
+        getAllModulesWithPendingRequestCount(isPending);
+      
+    }, [getAllModulesWithPendingRequestCount]);
+
+    useEffect(() => {
+        if (isGetAllModulesWithPendingRequestCountSucess && allGetAllModulesWithPendingRequestCountData) {
+          setModuleList(allGetAllModulesWithPendingRequestCountData);
+        }
+      }, [isGetAllModulesWithPendingRequestCountSucess, allGetAllModulesWithPendingRequestCountData]);
+
     useEffect(() => {
         if (moduleList && moduleList.length > 0) {
             setActiveModule(moduleList[0].moduleId);
@@ -27,7 +44,7 @@ const ModuleList = ({ moduleList, onModuleChange ,isPending}) => {
                     {moduleList && moduleList.map((data, index) => (
                         <li className={activeModule === data.moduleId ? "active" : ""}
                             onClick={() => handleModuleClick(data)} key={index}>
-                            <span>{data.moduleName}{ !isPending ? <div className="module-count">{data.requestCount}</div> : null }</span>
+                            <span>{data.moduleName} {data.requestCount > 0 ? <> <div className="module-count">{data.requestCount}</div></>: null}</span>
                             
                         </li> 
                     ))}
