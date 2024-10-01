7dc58c6be81ca498f4d5a6953234cd5ba2cc1e0d
Amit Sonigara <amit.s@mol-tech.com>
2024-10-01 16:21:00 +0530

    add new for GetSearchSuppliersDetailsByNameEmailWebsite and GetSearchCustomersDetailsByNameEmailWebsite

---
+++ OMS.API/OMS.Domain.Entities/API/Request/Customers/GetSearchCustomersDetailsByNameEmailWebsiteRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.Domain.Entities.API.Request.Customers
+{
+    public class GetSearchCustomersDetailsByNameEmailWebsiteRequest
+    {
+        public string? CustomerName { get; set; }
+        public string? Website { get; set; }
+        public string? EmailAddress { get; set; }
+    }
+}
