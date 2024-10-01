5f1ad6c8d7cf16f5ce5ed574fd0b9daa90514807
kirtan <kirtan.p@mol-tech.com>
2024-10-01 19:07:43 +0530

    update changes in  css

--- OMS Front End/src/components/ui/dropdown/DropDrown.js
+++ OMS Front End/src/components/ui/dropdown/DropDrown.js
@@ -15,7 +15,7 @@ const DropDown = (props) => {
 
   return (
     <>
-      <span className="d-inline-block">
+      <span className="d-inline-block custom-input">
         <DropdownSelect
           placeholder={props.placeholder}
           isMultiSelect={props.isMultiSelect}
