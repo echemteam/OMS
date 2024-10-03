aa0f42a9851527c85fcbcd706990345f6931400d
pankajchauhan <pankaj.c@mol-tech.com>
2024-10-03 10:23:11 +0530

    added array

--- OMS Front End/src/pages/order/orderDetail/feature/orderitemlist/OrderItemList.js
+++ OMS Front End/src/pages/order/orderDetail/feature/orderitemlist/OrderItemList.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Accordion } from "react-bootstrap";
 import CardSection from "../../../../../components/ui/card/CardSection";
 import Iconify from "../../../../../components/ui/iconify/Iconify";
 
 const OrderItemList = () => {
+  const [activeKey, setActiveKey] = useState([]);
+
+  const handleToggle = (key) => {
+    if (activeKey.includes(key)) {
+      setActiveKey(activeKey.filter((k) => k !== key));
+    } else {
+      setActiveKey([...activeKey, key]);
+    }
+  };
+  const data = [
+    {
+      eventKey: "0",
+      id: "Y-2520",
+      casNumber: "19679-75-5",
+      price: "$51.75",
+      quantity: "2 x 50MG",
+      totalCost: "1X$20",
+      status: "Complete",
+      statusClass: "complete-bg",
+      name: "2-amino-3 5-dibromobenzaldehyde",
+      mdlNumber: "12002452003584",
+      shippingAddress: "2-amino-3 5-dibromobenzaldehyde",
+      priority: "High",
+      requestDate: "10/28/2024",
+      promiseDate: "11/15/2024",
+    },
+    {
+      eventKey: "1",
+      id: "Y-2520",
+      casNumber: "19679-75-5",
+      price: "$51.75",
+      quantity: "2 x 50MG",
+      totalCost: "1X$20",
+      status: "Pending",
+      statusClass: "pending-bg",
+      name: "2-amino-3 5-dibromobenzaldehyde",
+      mdlNumber: "12002452003584",
+      shippingAddress: "2-amino-3 5-dibromobenzaldehyde",
+      priority: "High",
+      requestDate: "10/28/2024",
+      promiseDate: "11/15/2024",
+    },
+    // Add more data as needed
+  ];
   return (
     <div>
       <div className="order-item-list">
@@ -19,205 +63,118 @@ const OrderItemList = () => {
             </div>
           </div>
           <div className="accordian-desc">
-            <Accordion defaultActiveKey="0">
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>
-                  <div className="header-items">
-                    <span>Y-2520</span>
-                    <span>19679-75-5</span>
-                    <span>1X$20</span>
-                    <span>2 x 50MG</span>
-                    <span>$ 51.75</span>
-                    <span>
-                      <div className="status-btn complete-bg">Complete</div>
-                    </span>
-                  </div>
-                </Accordion.Header>
-                <Accordion.Body>
-                  <div className="row">
-                    <div className="col-xxl-6 col-lg-6 col-md-6 col-12">
-                      <div className="key-value-se">
-                        <span className="key-sec">Name</span>
-                        <span className="value-sec">
-                          &nbsp;:&nbsp; 2-amino-3 5-dibromobenzaldehyde
-                        </span>
-                      </div>
-                      <div className="key-value-se">
-                        <span className="key-sec">MDL Number </span>
-                        <span className="value-sec">
-                          &nbsp;:&nbsp; 12002452003584
-                        </span>
-                      </div>
-                      <div className="key-value-se">
-                        <span className="key-sec">Shipping Add.</span>
-                        <div className="value-right-btn">
-                          <span className="value-sec add-value">
-                            &nbsp;:&nbsp; 2-amino-3 5-dibromobenzaldehyde
+            <Accordion activeKey={activeKey}>
+              {data.map((item, index) => (
+                <Accordion.Item eventKey={item.eventKey} key={index}>
+                  <Accordion.Header onClick={() => handleToggle(item.eventKey)}>
+                    <div className="header-items">
+                      <span>{item.id}</span>
+                      <span>{item.casNumber}</span>
+                      <span>{item.totalCost}</span>
+                      <span>{item.quantity}</span>
+                      <span>{item.price}</span>
+                      <span>
+                        <div className={`status-btn ${item.statusClass}`}>
+                          {item.status}
+                        </div>
+                      </span>
+                    </div>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="row">
+                      <div className="col-xxl-6 col-lg-6 col-md-6 col-12">
+                        <div className="key-value-se">
+                          <span className="key-sec">Name</span>
+                          <span className="value-sec">
+                            &nbsp;:&nbsp; {item.name}
                           </span>
-                          <span className="right-btn">
-                            <span className="info-btn">
-                              <Iconify
-                                icon="ep:info-filled"
-                                className="swap-icon"
-                              />
+                        </div>
+                        <div className="key-value-se">
+                          <span className="key-sec">MDL Number </span>
+                          <span className="value-sec">
+                            &nbsp;:&nbsp; {item.mdlNumber}
+                          </span>
+                        </div>
+                        <div className="key-value-se">
+                          <span className="key-sec">Shipping Add.</span>
+                          <div className="value-right-btn">
+                            <span className="value-sec add-value">
+                              &nbsp;:&nbsp; {item.shippingAddress}
                             </span>
-                            <span className="info-btn tooltip-div">
-                              <Iconify
-                                icon="icon-park-outline:change"
-                                className="swap-icon"
-                              />
-                              <div className="tooltip-show">
-                                <p>Change Address</p>
+                            <span className="right-btn">
+                              <span className="info-btn">
+                                <Iconify
+                                  icon="ep:info-filled"
+                                  className="swap-icon"
+                                />
+                              </span>
+                              <span className="info-btn tooltip-div">
+                                <Iconify
+                                  icon="icon-park-outline:change"
+                                  className="swap-icon"
+                                />
+                                <div className="tooltip-show">
+                                  <p>Change Address</p>
+                                </div>
+                                <div className="tooltip-arrow-icon"></div>
+                              </span>
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-xxl-6 col-lg-6 col-md-6 col-12">
+                        <div className="accordian-right-full-sec">
+                          <div className="left-section">
+                            <div className="key-value-se">
+                              <span className="key-sec">Priority</span>
+                              <span className="value-sec">
+                                &nbsp;:&nbsp;
+                                <span className="status-btn heigh-bg">
+                                  {item.priority}
+                                </span>
+                              </span>
+                            </div>
+                            <div className="key-value-se">
+                              <span className="key-sec">Req-Date</span>
+                              <span className="value-sec">
+                                &nbsp;:&nbsp; {item.requestDate}
+                              </span>
+                            </div>
+                            <div className="key-value-se">
+                              <span className="key-sec">Promise Date</span>
+                              <span className="value-sec">
+                                &nbsp;:&nbsp; {item.promiseDate}
+                              </span>
+                            </div>
+                          </div>
+                          <div className="right-action-section">
+                            <div className="file-actions">
+                              <div className="btn-part pdf-view">
+                                <Iconify
+                                  icon="wpf:edit"
+                                  className="swap-icon"
+                                />
                               </div>
-                              <div className="tooltip-arrow-icon"></div>
-                            </span>
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-xxl-6 col-lg-6 col-md-6 col-12">
-                      <div className="accordian-right-full-sec">
-                        <div className="left-section">
-                          <div className="key-value-se">
-                            <span className="key-sec">Priority</span>
-                            <span className="value-sec">
-                              &nbsp;:&nbsp;{" "}
-                              <span class="status-btn heigh-bg">High</span>
-                            </span>
-                          </div>
-                          <div className="key-value-se">
-                            <span className="key-sec">Req-Date</span>
-                            <span className="value-sec">
-                              &nbsp;:&nbsp; 10/28/2024
-                            </span>
-                          </div>
-                          <div className="key-value-se">
-                            <span className="key-sec">Promise Date</span>
-                            <span className="value-sec">
-                              &nbsp;:&nbsp; 11/15/2024
-                            </span>
-                          </div>
-                        </div>
-                        <div className="right-action-section">
-                          <div class="file-actions">
-                            <div className="btn-part pdf-view">
-                              <Iconify icon="wpf:edit" className="swap-icon" />
-                            </div>
-                            <div className="btn-part dollar-view">
-                              <Iconify
-                                icon="mingcute:refund-dollar-line"
-                                className="swap-icon"
-                              />
-                            </div>
-                            <div className="btn-part delete-icon">
-                              <Iconify icon="mi:delete" className="swap-icon" />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>
-                  <div className="header-items">
-                    <span>Y-2520</span>
-                    <span>19679-75-5</span>
-                    <span>1X$20</span>
-                    <span>2 x 50MG</span>
-                    <span>$ 51.75</span>
-                    <span>
-                      <div className="status-btn pending-bg">Pending</div>
-                    </span>
-                  </div>
-                </Accordion.Header>
-                <Accordion.Body>
-                  <div className="row">
-                    <div className="col-xxl-6 col-lg-6 col-md-6 col-12">
-                      <div className="key-value-se">
-                        <span className="key-sec">Name</span>
-                        <span className="value-sec">
-                          &nbsp;:&nbsp; 2-amino-3 5-dibromobenzaldehyde
-                        </span>
-                      </div>
-                      <div className="key-value-se">
-                        <span className="key-sec">MDL Number </span>
-                        <span className="value-sec">
-                          &nbsp;:&nbsp; 12002452003584
-                        </span>
-                      </div>
-                      <div className="key-value-se">
-                        <span className="key-sec">Shipping Add.</span>
-                        <div className="value-right-btn">
-                          <span className="value-sec add-value">
-                            &nbsp;:&nbsp; 2-amino-3 5-dibromobenzaldehyde
-                          </span>
-                          <span className="right-btn">
-                            <span className="info-btn">
-                              <Iconify
-                                icon="ep:info-filled"
-                                className="swap-icon"
-                              />
-                            </span>
-                            <span className="info-btn tooltip-div">
-                              <Iconify
-                                icon="icon-park-outline:change"
-                                className="swap-icon"
-                              />
-                              <div className="tooltip-show">
-                                <p>Change Address</p>
+                              <div className="btn-part dollar-view">
+                                <Iconify
+                                  icon="mingcute:refund-dollar-line"
+                                  className="swap-icon"
+                                />
                               </div>
-                              <div className="tooltip-arrow-icon"></div>
-                            </span>
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-xxl-6 col-lg-6 col-md-6 col-12">
-                      <div className="accordian-right-full-sec">
-                        <div className="left-section">
-                          <div className="key-value-se">
-                            <span className="key-sec">Priority</span>
-                            <span className="value-sec">
-                              &nbsp;:&nbsp;{" "}
-                              <span class="status-btn heigh-bg">High</span>
-                            </span>
-                          </div>
-                          <div className="key-value-se">
-                            <span className="key-sec">Req-Date</span>
-                            <span className="value-sec">
-                              &nbsp;:&nbsp; 10/28/2024
-                            </span>
-                          </div>
-                          <div className="key-value-se">
-                            <span className="key-sec">Promise Date</span>
-                            <span className="value-sec">
-                              &nbsp;:&nbsp; 11/15/2024
-                            </span>
-                          </div>
-                        </div>
-                        <div className="right-action-section">
-                          <div class="file-actions">
-                            <div className="btn-part pdf-view">
-                              <Iconify icon="wpf:edit" className="swap-icon" />
-                            </div>
-                            <div className="btn-part dollar-view">
-                              <Iconify
-                                icon="mingcute:refund-dollar-line"
-                                className="swap-icon"
-                              />
-                            </div>
-                            <div className="btn-part delete-icon">
-                              <Iconify icon="mi:delete" className="swap-icon" />
+                              <div className="btn-part delete-icon">
+                                <Iconify
+                                  icon="mi:delete"
+                                  className="swap-icon"
+                                />
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </Accordion.Body>
-              </Accordion.Item>
+                  </Accordion.Body>
+                </Accordion.Item>
+              ))}
             </Accordion>
           </div>
         </CardSection>
