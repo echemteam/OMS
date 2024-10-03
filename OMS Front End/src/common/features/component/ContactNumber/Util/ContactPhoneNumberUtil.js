99408accca6c35ad1ac0f5b5c8d39d3419cae6ec
shrutenparmar <shruten.p@mol-tech.com>
2024-10-03 15:54:38 +0530

    Contact phone number issue

--- OMS Front End/src/common/features/component/ContactNumber/Util/ContactPhoneNumberUtil.js
+++ OMS Front End/src/common/features/component/ContactNumber/Util/ContactPhoneNumberUtil.js
@@ -1,5 +1,6 @@
 import ToastService from "../../../../../services/toastService/ToastService";
 import PropTypes from 'prop-types';
+
 export const addPhoneNumberData = (data, contactId, listData, setListData, successMessage, maxLengthMessage, duplicateMessage, onResetData, onSuccess) => {
     const phoneCode = data.phoneNumber.phoneCode && typeof data.phoneNumber === "object" ? data.phoneNumber.phoneCode : data.phoneCode;
     const fullPhoneNumber = data.phoneNumber.PhoneNumber && typeof data.phoneNumber === "object" ? data.phoneNumber.PhoneNumber : data.phoneNumber;
@@ -67,9 +68,7 @@ export const updatePhoneNumberData = (data, listData, setListData, successMessag
         const normalizedPhoneCode = `+${phoneCode}`;
 
         // Remove phoneCode from phoneNumber if it starts with the normalized phoneCode
-        const phoneNumberWithoutCode = fullPhoneNumber.startsWith(normalizedPhoneCode)
-            ? fullPhoneNumber.slice(normalizedPhoneCode.length)
-            : fullPhoneNumber;
+        const phoneNumberWithoutCode = fullPhoneNumber.startsWith(normalizedPhoneCode) ? fullPhoneNumber.slice(normalizedPhoneCode.length) : fullPhoneNumber;
         const phoneTypeId = data.phoneTypeId && typeof data.phoneTypeId === "object" ? data.phoneTypeId.value : data.phoneTypeId
         const phoneType = data.phoneTypeId.label ? data.phoneTypeId.label : data.phoneType
         const isPrimary = data.isPrimaryPhoneNumber
@@ -89,6 +88,9 @@ export const updatePhoneNumberData = (data, listData, setListData, successMessag
                         isPrimary: isPrimary
                     };
                 } else if (isPrimary) {
+                    const phoneCode = item.phoneNumber.phoneCode && typeof item.phoneNumber === "object" ? item.phoneNumber.phoneCode : item.phoneCode;
+                    const fullPhoneNumber = item.phoneNumber.PhoneNumber && typeof item.phoneNumber === "object" ? item.phoneNumber.PhoneNumber : item.phoneNumber;
+                    const phoneNumberWithoutCode = fullPhoneNumber.startsWith(normalizedPhoneCode) ? fullPhoneNumber.slice(normalizedPhoneCode.length) : fullPhoneNumber;
                     return {
                         ...item,
                         phoneCode: phoneCode,
@@ -96,6 +98,9 @@ export const updatePhoneNumberData = (data, listData, setListData, successMessag
                         isPrimary: false
                     };
                 } else {
+                    const phoneCode = item.phoneNumber.phoneCode && typeof item.phoneNumber === "object" ? item.phoneNumber.phoneCode : item.phoneCode;
+                    const fullPhoneNumber = item.phoneNumber.PhoneNumber && typeof item.phoneNumber === "object" ? item.phoneNumber.PhoneNumber : item.phoneNumber;
+                    const phoneNumberWithoutCode = fullPhoneNumber.startsWith(normalizedPhoneCode) ? fullPhoneNumber.slice(normalizedPhoneCode.length) : fullPhoneNumber;
                     return {
                         ...item,
                         phoneCode: phoneCode,
