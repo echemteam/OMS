200a0be6ce312e33555227408056a0521ac1c17c
shrutenparmar <shruten.p@mol-tech.com>
2024-10-04 19:20:57 +0530

    REMOVE VALIDAITON

--- OMS Front End/src/pages/supplierDetails/feature/supplierBasicDetail/config/SupplierBasicDetail.data.js
+++ OMS Front End/src/pages/supplierDetails/feature/supplierBasicDetail/config/SupplierBasicDetail.data.js
@@ -56,7 +56,6 @@ export const supplierBasicData = {
         isCustomButtonVisible: false,
         acceptedFiles: '.png , .jpg ',
       },
-      validation: [{ type: "require" }],
       style: {
         containerCss: "col-xxl-4 col-xl-4 col-md-4 col-4 col-4 mb-input mb-0 custom-file-upload-section",
       },
