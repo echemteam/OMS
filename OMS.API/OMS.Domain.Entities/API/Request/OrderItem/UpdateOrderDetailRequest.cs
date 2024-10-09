8eb31c822fd91a1f133ce532a05eb1095b704aa3
Amit Sonigara <amit.s@mol-tech.com>
2024-10-09 10:28:47 +0530

    change api UpdateOrderDetail.

--- OMS.API/OMS.Domain.Entities/API/Request/OrderItem/UpdateOrderDetailRequest.cs
+++ OMS.API/OMS.Domain.Entities/API/Request/OrderItem/UpdateOrderDetailRequest.cs
@@ -7,6 +7,8 @@
         public byte? OrderMethodId { get; set; }
         public DateTime? OrderReceivedDate { get; set; }
         public string? ReferenceNumber { get; set; }
+        public int? CustomerId { get; set; }
+        public string? PoNumber { get; set; }
 
     }
 }
