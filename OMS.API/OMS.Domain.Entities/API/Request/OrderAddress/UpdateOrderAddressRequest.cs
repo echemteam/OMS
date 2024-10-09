82a2cdd5f5b0c4df87dc60a5a1eb65a4f47e1aee
Amit Sonigara <amit.s@mol-tech.com>
2024-10-09 14:37:03 +0530

    chnage UpdateOrderAddressRequest.

--- OMS.API/OMS.Domain.Entities/API/Request/OrderAddress/UpdateOrderAddressRequest.cs
+++ OMS.API/OMS.Domain.Entities/API/Request/OrderAddress/UpdateOrderAddressRequest.cs
@@ -5,7 +5,7 @@
         public int? OrderAddressId { get; set; }
         public int? OrderId { get; set; }
         public int? BillingAddressId { get; set; }
-        public int? ShippingAddressId { get; set; } 
-        public int? OrderItemId { get; set; } 
+        public int? ShippingAddressId { get; set; }
+        public int? OrderItemId { get; set; } = 0;
     }
 }
