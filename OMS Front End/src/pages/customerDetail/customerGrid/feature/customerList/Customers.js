1c0a4f5f4af881e03ffe24c2a371985804c785f8
kirtan <kirtan.p@mol-tech.com>
2024-10-16 10:41:01 +0530

    added key code enums

--- OMS Front End/src/pages/customerDetail/customerGrid/feature/customerList/Customers.js
+++ OMS Front End/src/pages/customerDetail/customerGrid/feature/customerList/Customers.js
@@ -13,6 +13,7 @@ import CustomerListContext from "../../../../../utils/ContextAPIs/Customer/Custo
 import { AllCustomerGridConfig, ApprovedCustomerGridConfig, PendingCustomerGridConfig, RejectedCustomerGridConfig, SubmittedCustomerGridConfig } from "../../../../../common/features/component/CustomerSupplierListConfig/CustomerSupplierListConfig.data";
 import InActiveCustomerTab from "../customerInActiveTabs/InActiveCustomerTab";
 import { useLocation } from "react-router-dom";
+import KeyCodes from "../../../../../utils/Enums/KeyCodesEnums";
 
 const Customers = () => {
   const listRef = useRef();
@@ -117,7 +118,7 @@ const Customers = () => {
   };
 
   const handleKeyPress = (event) => {
-    if (event.code === "Enter") {
+    if (event.code === KeyCodes.ENTER) {
       handleSearch();
     }
   }
