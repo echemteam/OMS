0b075e972b7d6a9cb0b500a9b22a1654b1d93610
shrutenparmar <shruten.p@mol-tech.com>
2024-10-18 14:11:34 +0530

    Payment info chnages

--- OMS Front End/src/pages/order/orderDetail/feature/paymentinformation/PaymentInformation.js
+++ OMS Front End/src/pages/order/orderDetail/feature/paymentinformation/PaymentInformation.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
-import { zone } from "./Config/Payment.Data";
+import { COUNTRYUSA, ZoneEnum } from "./Config/Payment.Data";
 import CardSection from "../../../../../components/ui/card/CardSection";
 import DataLoader from "../../../../../components/ui/dataLoader/DataLoader";
 import NoRecordFound from "../../../../../components/ui/noRecordFound/NoRecordFound";
@@ -39,19 +39,19 @@ const PaymentInformation = ({ orderDetails }) => {
       setFinancialInfo(data);
     }
   }, [isGetDetailsbyCustomerID, data]);
-  
+
   useEffect(() => {
     if (!isGetDefaultValueFetching && isGetDefaultValueSuccess && isGetDefaultValueData && isgetRegisteredAddressesSuccess && isgetRegisteredAddressesData) {
       setAccountType(isGetDefaultValueData.name);
       if (isGetDefaultValueData?.deliveryMethodsList?.length > 0 && isgetRegisteredAddressesData.registeredAddress.countryId && isgetRegisteredAddressesData.registeredAddress.countryName) {
         const countryId = isgetRegisteredAddressesData.registeredAddress.countryId;
         const countryName = isgetRegisteredAddressesData.registeredAddress.countryName.trim();
-        const isCountryUSA = countryId === 233 && countryName.toLowerCase() === countryName.toLowerCase();
+        const isCountryUSA = countryId === 233 && countryName.toLowerCase() === COUNTRYUSA.toLowerCase();
         const primaryDeliveryMethod = isGetDefaultValueData?.deliveryMethodsList?.find(data => data.isPrimary && data?.isForInternational === !isCountryUSA);
         if (primaryDeliveryMethod) {
           const updatedPrimaryDeliveryMethod = {
             ...primaryDeliveryMethod,
-            zone: primaryDeliveryMethod.isForInternational ? zone.International : zone.Domestic
+            zone: primaryDeliveryMethod.isForInternational ? ZoneEnum.INTERNATIONAL : ZoneEnum.DOMESTIC
           };
           setDeliveryDetails(updatedPrimaryDeliveryMethod);
         } else {
@@ -80,127 +80,125 @@ const PaymentInformation = ({ orderDetails }) => {
 
   return (
     <div>
-      {!isGetDetailsbyCustomerIdFetching && !isGetDefaultValueFetching ?
-        <CardSection cardTitle="Payment Information">
-          <div className="row">
-            <div className="col-xxl-6 col-lg-6 col-md-6 col-12">
-              {!isGetDetailsbyCustomerIdFetching ?
-                <div className="financial-section">
-                  <div className="financial-label">Financial</div>
-                  <div className="financial-keyvalue-pair">
-                    <div className="financial-key">Default Payment Terms :</div>
-                    <div className="financial-value">{getPaymentTerm(financialinancialInfo.paymentTermId) || "N/A"}</div>
-                  </div>
-                  <div className="financial-keyvalue-pair">
-                    <div className="financial-key">Credit Limit :</div>
-                    <div className="financial-value">{financialinancialInfo.creditLimit !== undefined ? financialinancialInfo.creditLimit : "N/A"}</div>
-                  </div>
-                  <div className="financial-keyvalue-pair">
-                    <div className="financial-key">Payment Method :</div>
-                    <div className="financial-value">{getPaymentMethod(financialinancialInfo.paymentMethodId) || "N/A"}</div>
-                  </div>
-                  {financialinancialInfo.paymentMethodId === 4 && (
-                    <div className="financial-keyvalue-pair">
-                      <div className="financial-key">Card Processing Charge :</div>
-                      <div className="financial-value">
-                        {financialinancialInfo.cardProcessingCharges !== undefined ? financialinancialInfo.cardProcessingCharges : "N/A"}
-                      </div>
-                    </div>
-                  )}
-                  <div className="financial-keyvalue-pair">
-                    <div className="financial-key">Exempt Sales Tax :</div>
-                    <div className="financial-value"> <input
-                      type="checkbox"
-                      checked={financialinancialInfo.exemptSalesTax}
-                      readOnly
-                    /></div>
-                  </div>
-                  {financialinancialInfo.exemptSalesTax === false && (
-                    <div className="financial-keyvalue-pair">
-                      <div className="financial-key">Sales Tax :</div>
-                      <div className="financial-value">
-                        {financialinancialInfo.salesTax !== undefined ? financialinancialInfo.salesTax : "N/A"}
-                      </div>
-                    </div>
-                  )}
-                  {financialinancialInfo.bankWireFee !== null && financialinancialInfo.bankWireFee !== undefined  && (
-                    <div className="financial-keyvalue-pair">
-                      <div className="financial-key">Bank Wire Fee:</div>
-                      <div className="financial-value">
-                        {financialinancialInfo.bankWireFee}
-                      </div>
-                    </div>
-                  )}
+      <CardSection cardTitle="Payment Information">
+        <div className="row">
+          <div className="col-xxl-6 col-lg-6 col-md-6 col-12">
+            {!isGetDetailsbyCustomerIdFetching && financialinancialInfo ?
+              <div className="financial-section">
+                <div className="financial-label">Financial</div>
+                <div className="financial-keyvalue-pair">
+                  <div className="financial-key">Default Payment Terms :</div>
+                  <div className="financial-value">{getPaymentTerm(financialinancialInfo.paymentTermId) || "N/A"}</div>
                 </div>
-                : <DataLoader />
-              }
-            </div>
-            <div className="col-xxl-6 col-lg-6 col-md-6 col-12">
+                <div className="financial-keyvalue-pair">
+                  <div className="financial-key">Credit Limit :</div>
+                  <div className="financial-value">{financialinancialInfo.creditLimit !== undefined ? financialinancialInfo.creditLimit : "N/A"}</div>
+                </div>
+                <div className="financial-keyvalue-pair">
+                  <div className="financial-key">Payment Method :</div>
+                  <div className="financial-value">{getPaymentMethod(financialinancialInfo.paymentMethodId) || "N/A"}</div>
+                </div>
+                {financialinancialInfo.paymentMethodId === 4 && (
+                  <div className="financial-keyvalue-pair">
+                    <div className="financial-key">Card Processing Charge :</div>
+                    <div className="financial-value">
+                      {financialinancialInfo.cardProcessingCharges !== undefined ? financialinancialInfo.cardProcessingCharges : "N/A"}%
+                    </div>
+                  </div>
+                )}
+                <div className="financial-keyvalue-pair">
+                  <div className="financial-key">Exempt Sales Tax :</div>
+                  <div className="financial-value"> <input
+                    type="checkbox"
+                    checked={financialinancialInfo.exemptSalesTax}
+                    readOnly
+                  /></div>
+                </div>
+                {financialinancialInfo.exemptSalesTax === false && (
+                  <div className="financial-keyvalue-pair">
+                    <div className="financial-key">Sales Tax :</div>
+                    <div className="financial-value">
+                      {financialinancialInfo.salesTax !== undefined ? financialinancialInfo.salesTax : "N/A"}%
+                    </div>
+                  </div>
+                )}
+                {financialinancialInfo.bankWireFee !== null && financialinancialInfo.bankWireFee !== undefined && (
+                  <div className="financial-keyvalue-pair">
+                    <div className="financial-key">Bank Wire Fee:</div>
+                    <div className="financial-value">
+                      ${financialinancialInfo.bankWireFee}
+                    </div>
+                  </div>
+                )}
+              </div>
+              : <DataLoader />
+            }
+          </div>
+          <div className="col-xxl-6 col-lg-6 col-md-6 col-12">
+            {!isGetDefaultValueFetching && accountType ?
               <div className="shipping-section">
                 <div className="shipping-label">Shipping</div>
-                {!isGetDefaultValueFetching ?
-                  <React.Fragment>
-                    <div className="shipping-keyvalue-pair">
-                      <div className="shipping-key">Account Type :</div>
-                      <div className="shipping-value">{accountType}</div>
-                    </div>
-                    {carriersDetails && carriersDetails.carrier &&
-                      <>
-                        <div className="shipping-sub-label">Carrier Details</div>
-                        <div className="custom-row">
-                          <div className="custom-col">
-                            <div className="shipping-sub-keyvalue-pair">
-                              <div className="shipping-sub-key">Carrier :</div>
-                              <div className="shipping-sub-value">{carriersDetails.carrier}</div>
-                            </div>
-                            <div className="shipping-sub-keyvalue-pair">
-                              <div className="shipping-sub-key">Handling Fee :</div>
-                              <div className="shipping-sub-value">${carriersDetails.handlingFee?.toFixed(2)}</div>
-                            </div>
+                <React.Fragment>
+                  <div className="shipping-keyvalue-pair">
+                    <div className="shipping-key">Account Type :</div>
+                    <div className="shipping-value">{accountType}</div>
+                  </div>
+                  {carriersDetails && carriersDetails.carrier &&
+                    <>
+                      <div className="shipping-sub-label">Carrier Details</div>
+                      <div className="custom-row">
+                        <div className="custom-col">
+                          <div className="shipping-sub-keyvalue-pair">
+                            <div className="shipping-sub-key">Carrier :</div>
+                            <div className="shipping-sub-value">{carriersDetails.carrier}</div>
                           </div>
-                          <div className="custom-col">
-                            <div className="shipping-sub-keyvalue-pair">
-                              <div className="shipping-sub-key">Account Number :</div>
-                              <div className="shipping-sub-value">{carriersDetails.accountNumber}</div>
-                            </div>
+                          <div className="shipping-sub-keyvalue-pair">
+                            <div className="shipping-sub-key">Handling Fee :</div>
+                            <div className="shipping-sub-value">${carriersDetails.handlingFee?.toFixed(2)}</div>
                           </div>
                         </div>
-                      </>
-                    }
-                    {deliveryDetails && deliveryDetails.zone &&
-                      <>
-                        <div className="shipping-sub-label">Delivery Method Details</div>
-                        <div className="custom-row">
-                          <div className="custom-col">
-                            <div className="shipping-sub-keyvalue-pair">
-                              <div className="shipping-sub-key">Zone :</div>
-                              <div className="shipping-sub-value">{deliveryDetails.zone}</div>
-                            </div>
-                            <div className="shipping-sub-keyvalue-pair">
-                              <div className="shipping-sub-key">Charge :</div>
-                              <div className="shipping-sub-value">$ {deliveryDetails.charge?.toFixed(2)}</div>
-                            </div>
-                          </div>
-                          <div className="custom-col">
-                            <div className="shipping-sub-keyvalue-pair">
-                              <div className="shipping-sub-key">Charge Type :</div>
-                              <div className="shipping-sub-value">{deliveryDetails.name}</div>
-                            </div>
+                        <div className="custom-col">
+                          <div className="shipping-sub-keyvalue-pair">
+                            <div className="shipping-sub-key">Account Number :</div>
+                            <div className="shipping-sub-value">{carriersDetails.accountNumber}</div>
                           </div>
                         </div>
-                      </>
-                    }
-                    {!carriersDetails?.carrier && !deliveryDetails?.zone && accountType &&
-                      <NoRecordFound message="No Primary Record Found" />
-                    }
-                  </React.Fragment>
-                  : <DataLoader />
-                }
+                      </div>
+                    </>
+                  }
+                  {deliveryDetails && deliveryDetails.zone &&
+                    <>
+                      <div className="shipping-sub-label">Delivery Method Details</div>
+                      <div className="custom-row">
+                        <div className="custom-col">
+                          <div className="shipping-sub-keyvalue-pair">
+                            <div className="shipping-sub-key">Zone :</div>
+                            <div className="shipping-sub-value">{deliveryDetails.zone}</div>
+                          </div>
+                          <div className="shipping-sub-keyvalue-pair">
+                            <div className="shipping-sub-key">Charge :</div>
+                            <div className="shipping-sub-value">${deliveryDetails.charge?.toFixed(2)}</div>
+                          </div>
+                        </div>
+                        <div className="custom-col">
+                          <div className="shipping-sub-keyvalue-pair">
+                            <div className="shipping-sub-key">Charge Type :</div>
+                            <div className="shipping-sub-value">{deliveryDetails.name}</div>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  }
+                  {!carriersDetails?.carrier && !deliveryDetails?.zone && accountType &&
+                    <NoRecordFound message="No Primary Record Found" />
+                  }
+                  {!accountType && <NoRecordFound />}
+                </React.Fragment>
               </div>
-            </div>
+              : <DataLoader />}
           </div>
-        </CardSection>
-        : <DataLoader />}
+        </div>
+      </CardSection>
     </div>
   );
 };
